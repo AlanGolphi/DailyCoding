86b34d247649dec4ebfc906629afbe4ba8b288b5
AlanGolphi <alangolphi@163.com>
2024-07-09 18:27:49 +0800

    solve some problems

---
+++ sourceFile/typescript/100_SameTree.ts
@@ -0,0 +1,29 @@
+// @algorithm @lc id=100 lang=typescript
+// @title same-tree
+// @test([1,2,3],[1,2,3])=true
+// @test([1,2],[1,null,2])=false
+// @test([1,2,1],[1,1,2])=false
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+
+  return (
+    p.val === q.val &&
+    isSameTree(q.left, q.left) &&
+    isSameTree(p.right, q.right)
+  );
+}
