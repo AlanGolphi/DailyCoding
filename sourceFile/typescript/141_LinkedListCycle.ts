ce4b3fb1c142436869bed46c2667156afab4db2f
AlanGolphi <alangolphi@163.com>
2024-11-22 03:22:14 +0000

    repactice leetcode 141 and add test case

--- sourceFile/typescript/141_LinkedListCycle.ts
+++ sourceFile/typescript/141_LinkedListCycle.ts
@@ -1,3 +1,5 @@
+import { createList, ListNode } from "./models";
+
 // @algorithm @lc id=141 lang=typescript
 // @title linked-list-cycle
 // @test([3,2,0,-4],1)=true
@@ -15,28 +17,19 @@
  * }
  */
 
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
+// function hasCycle(head: ListNode | null): boolean {
+//   if (!head || !head.next) return false;
+//   let slow = head;
+//   let fast = head;
+//   while (slow.next && fast.next && fast.next.next) {
+//     if (fast.next.next === slow) return true;
+//     slow = slow.next;
+//     fast = fast.next.next;
+//   }
+//   return false;
+// }
 
 function hasCycle(head: ListNode | null): boolean {
-  if (!head || !head.next) return false;
-  let slow = head;
-  let fast = head;
-  while (slow.next && fast.next && fast.next.next) {
-    if (fast.next.next === slow) return true;
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-  return false;
-}
-
-function hasCycle1(head: ListNode | null): boolean {
   if (!head || !head.next) return false;
   const cache = new Set<ListNode | null>();
   let p = head;
@@ -47,3 +40,14 @@ function hasCycle1(head: ListNode | null): boolean {
   }
   return false;
 }
+
+const testHead = createList([3, 2, 0, -4]);
+let tail = testHead;
+while (tail?.next) {
+  tail = tail.next;
+}
+if (tail) {
+  tail.next = testHead;
+}
+
+console.log(hasCycle(testHead));
