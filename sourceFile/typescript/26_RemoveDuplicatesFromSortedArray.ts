4ca9bb7709bb51cfcc3402919a17c7b8899527e8
AlanGolphi <alangolphi@163.com>
2024-12-30 07:09:28 +0000

    repractice leetcode 27

--- sourceFile/typescript/26_RemoveDuplicatesFromSortedArray.ts
+++ sourceFile/typescript/26_RemoveDuplicatesFromSortedArray.ts
@@ -1,27 +1,42 @@
-// @algorithm @lc id=26 lang=typescript 
+// @algorithm @lc id=26 lang=typescript
 // @title remove-duplicates-from-sorted-array
 // @test([1,1,2])=2
 // @test([0,0,1,1,1,2,2,3,3,4])=5
-import {excuteWithTimeAndMemory} from './utils'
+import { excuteWithTimeAndMemory } from "./utils";
 
 function removeDuplicates(nums: number[]): number {
-  if (nums.length <= 1) return nums.length
-  let count = 0
+  if (nums.length <= 1) return nums.length;
+  let count = 0;
   for (let i = 1; i < nums.length; i++) {
     if (nums[count] !== nums[i]) {
-      count++
-      nums[count] = nums[i]
+      count++;
+      nums[count] = nums[i];
     }
   }
 
-  return count + 1
-};
+  return count + 1;
+}
 
-let nums1 = [0,0,1,1,1,2,2,3,3,4]
+function removeDuplicates1(nums: number[]): number {
+  const record: Set<number> = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (!record.has(nums[i])) {
+      record.add(nums[i]);
+    }
+  }
+  const arr = Array.from(record);
+  for (let i = 0; i < arr.length; i++) {
+    nums[i] = arr[i];
+  }
+
+  return arr.length;
+}
+
+let nums1 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 // let nums1 = [1,1,2]
 // let nums1 = [0]
 
 excuteWithTimeAndMemory(() => {
-  console.log(removeDuplicates(nums1))
-  console.log(nums1)
-}, 'remove duplicates')
+  console.log(removeDuplicates(nums1));
+  console.log(nums1);
+}, "remove duplicates");
